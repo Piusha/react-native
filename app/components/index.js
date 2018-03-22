6fe892f35efa26fdc19544abe2db28f0809b969c
Piusha Kalyana <piusha.k@softcodeit.com>
2018-03-22 23:21:59 +0530

    Buttons

---
+++ app/components/index.js
@@ -0,0 +1,8 @@
+import ButtonBasics from './buttonBasic';
+import FlatListBasics from './flatListBasics';
+import SectionListBasics from './SectionListBasics';
+export  {
+    ButtonBasics,
+    FlatListBasics,
+    SectionListBasics
+}
